ecada41c42c98356c6ea617e18a45fb08333558e
Pawar187 <pawaranand187100@gmail.com>
2024-06-19 23:57:11 +0530

    first commit

---
+++ src/testDataProvider/DataProviderParameterExample.java
@@ -0,0 +1,21 @@
+package testDataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviderParameterExample {
+	
+	@Test(dataProvider = "scenarioData", dataProviderClass=DataProviderSource.class)
+	public void scenario1(String scenarioData) {
+		System.out.println("Scenario testing: Data(" + scenarioData + ")");
+	}
+	
+	@Test(dataProvider = "scenarioData", dataProviderClass=DataProviderSource.class)
+	public void scenario2(String scenarioData) {
+		System.out.println("Scenario testing: Data(" + scenarioData + ")");
+	}
+	
+	@Test(dataProvider = "scenarioData", dataProviderClass=DataProviderSource.class)
+	public void commonScenarios(String scenarioData) {
+		System.out.println("Common Scenarios testing: Data(" + scenarioData + ")");
+	}
+
+}
